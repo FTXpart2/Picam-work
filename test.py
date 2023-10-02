7b36459b4499930e1617330ff94b3f204a15d542
FTXpart2 <122008092+FTXpart2@users.noreply.github.com>
2023-10-02 00:07:46 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from picamera2 import Picamera2, Preview
 import time
 
+
 picam2 = Picamera2()
+picam2.resolution = (640, 480)
+picam2.framerate = 15
 picam2.start_preview(Preview.NULL)
+
 for x in range(0,10):
     picam2.start_and_capture_file("wes-{}.jpg".format(x) )
     time.sleep(30)
